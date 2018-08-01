f079af2c548f3fb7adcfcf0fe44b36111f629abe
Hamid Mirzaei <hmirzai@gmail.com>
2018-08-01 16:17:08 -0700

    add more commands to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cp .emacs ~
 cp .inputrc ~
 
 echo 'alias emacs="emacs -nw"' >> ~/.bashrc
-echo 'GAZEBO_MODEL_PATH=~/catkin_ws/src/RoboND-Perception-Project/pr2_robot/models:$GAZEBO_MODEL_PATH' >> ~/.bashrc
+echo 'export GAZEBO_MODEL_PATH=~/catkin_ws/src/RoboND-Perception-Project/pr2_robot/models:$GAZEBO_MODEL_PATH' >> ~/.bashrc
 echo 'source ~/catkin_ws/devel/setup.bash' >> ~/.bashrc
 echo 'Xft.dpi: 170' > ~/Xresources
 
@@ -13,6 +13,10 @@ echo 'Xft.dpi: 170' > ~/Xresources
 catkin_make
 rosdep install --from-paths src --ignore-src --rosdistro=kinetic -y
 catkin_make
+
+pip install numpy scipy sklearn
+git config --global user.email "hmirzai@gmail.com"
+git config --global user.name "Hamid Mirzaei"
 
 #roslaunch sensor_stick training.launch
 #roslaunch sensor_stick training.launch
